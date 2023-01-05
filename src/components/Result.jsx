317b439704e1c4635d9d580e0b5048fa2ee14203
Abdo dsouky <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-01-05 20:51:27 +0200

    fix href

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,11 @@
-export default function Result({ countCorrectAnswers }) {
+export default function Result({ setCategory, countCorrectAnswers }) {
     return (
         <>
             <p>
                 You got <strong>{countCorrectAnswers}</strong> correct!
             </p>
             <p>Thank you for playing!</p>
-            <a href='/' class='btn btn-success stretched-link'>
+            <a href='/Quiz-app' class='btn btn-success stretched-link'>
                 Return To Home Page
             </a>
 
